2a4926d911c6b9c645a4d5434009861a51fee209
ansakhalid-01 <144807754+ansakhalid-01@users.noreply.github.com>
2024-09-20 20:52:31 +0500

    Update Logo

--- src/components/header/Logo.jsx
+++ src/components/header/Logo.jsx
@@ -1,11 +1,14 @@
 // src/Logo.js
 import React from "react";
+import { Link } from "react-router-dom"; // Import Link for navigation
 import "./Logo.css";
 
 function Logo() {
   return (
     <div className="logo-container">
-      <img src="/logo.svg" alt="Logo" className="logo-image" />
+      <Link to="/">
+        <img src="/logo.svg" alt="Logo" className="logo-image" />
+      </Link>
     </div>
   );
 }
