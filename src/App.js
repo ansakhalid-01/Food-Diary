11cf824b20611322ee9f647ca34d3109fa718d45
ansakhalid-01 <144807754+ansakhalid-01@users.noreply.github.com>
2024-09-20 13:41:50 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import Router components
 import './App.css';
+import Navbar from './components/navbar/Navbar';
+import RecipeItems from './components/recipe/RecipeItems';
+import Footer from './components/footer/Footer';
+import RecipeDetail from './components/recipe/RecipeDetails'; // New component for recipe details
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        
+        {/* Define the routes here */}
+        <Routes>
+          <Route
+            path="/" // Default route (Homepage)
+            element={
+              <>
+                <RecipeItems />
+              </>
+            }
+          />
+          
+          {/* Define a dynamic route for recipe details */}
+          <Route path="/recipe/:id" element={<RecipeDetail />} />
+        </Routes>
+
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/header/Logo.jsx
@@ -0,0 +1,13 @@
+// src/Logo.js
+import React from "react";
+import "./Logo.css";
+
+function Logo() {
+  return (
+    <div className="logo-container">
+      <img src="/logo.svg" alt="Logo" className="logo-image" />
+    </div>
+  );
+}
+
+export default Logo;
